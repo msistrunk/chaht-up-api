2fd2ce47f9a80339aead7b5062176a0b90f135b9
Mitchell Sistrunk <sistrunkmitchell@gmail.com>
2019-05-26 15:40:38 -0500

    Add Socket IO and demo

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var chatRouter = require('./routes/chat');
 
 var app = express();
+var server = app.listen(3001);
+var io = require('socket.io').listen(server); // this tells socket.io to use our express 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/chat', chatRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -36,6 +40,12 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+io.sockets.on('connection', function (socket) {
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
 
 module.exports = app;
--- routes/chat.js
+++ routes/chat.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+var directory = path.basename(__dirname);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //res.sendFile(path.join(__dirname, '..', '/views/chat.html'));
+  res.render('chat');
 });
 
 module.exports = router;
