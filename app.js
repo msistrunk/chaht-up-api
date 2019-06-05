72317ace33302ae55b50d8a67707993b83765d9d
Mitchell Sistrunk <sistrunkmitchell@gmail.com>
2019-06-05 00:27:43 -0500

    Add api endpoints for storing and retrieving messages

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var chatRouter = require('./routes/chat');
+var messagesRouter = require('./routes/messages');
 
 var app = express();
 var server = app.listen(3001);
 var io = require('socket.io').listen(server); // this tells socket.io to use our express 
+app.io = io;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/chat', chatRouter);
+app.use('/messages', messagesRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/messages.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+var mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://msistrunk:chahtupdev@cluster0-k5m3m.mongodb.net/chahtup?retryWrites=true&w=majority", {useNewUrlParser: true});
+
+var messageSchema = new mongoose.Schema({
+  author: String,
+  message: String
+ });
+ var Message = mongoose.model("Message", messageSchema);
+
+/* POST messages. */
+router.post('/', function(req, res) {
+  var myMessage = new Message(req.body);
+  console.log(req.body);
+  myMessage.save()
+  .then(item => {
+    req.app.io.emit('chat message', item.message);
+    res.send("Message saved");
+  })
+  .catch(err => {
+    res.status(400).send("unable to save to database");
+  })
+});
+
+router.get('/', function(req, res) {
+  Message.find({}, (err, messages) => {
+    res.json(messages)
+  })
+})
+
+module.exports = router;
